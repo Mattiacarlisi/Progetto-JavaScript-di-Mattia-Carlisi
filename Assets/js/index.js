1e81ad056070196fea0a1cd0a93ae29b01e497e1
mattia <carlisimattia@gmail.com>
2024-11-23 13:03:52 +0100

    22

--- Assets/js/index.js
+++ Assets/js/index.js
@@ -19,7 +19,7 @@ document.addEventListener('DOMContentLoaded', () => {
     let currentImageIndex = parseInt(localStorage.getItem('imageIndex')) || 0;
     let counterValue = parseInt(localStorage.getItem('counterValue')) || 0;
 
-    // Crea e aggiorna l'elemento display
+    
     const counterContainer = document.querySelector('#counterContainer');
     const display = createElement('p', { className: 'totale', innerHTML: counterValue, attributes: { id: 'totale' } });
     counterContainer.append(
@@ -28,11 +28,11 @@ document.addEventListener('DOMContentLoaded', () => {
         createElement('button', { className: 'piu-btn', innerHTML: '+', attributes: { 'data-action': 'increment' } })
     );
 
-    // Funzione per aggiornare lo stato
+ 
     const updateState = (increment) => {
         counterValue = increment === null ? 0 : counterValue + increment;
         currentImageIndex = (currentImageIndex + (increment || 0) + imageList.length) % imageList.length;
-        display.textContent = counterValue; // Aggiorna il valore del contatore
+        display.textContent = counterValue; 
         document.querySelector('#memeImg').src = imageList[currentImageIndex];
         localStorage.setItem('counterValue', counterValue);
         localStorage.setItem('imageIndex', currentImageIndex);
@@ -50,7 +50,7 @@ document.addEventListener('DOMContentLoaded', () => {
         document.querySelector('#totale').style.color = isDay ? 'yellow' : 'black';
     };
 
-    // Aggiungi gli event listener
+    
     document.body.addEventListener('click', (e) => {
         const action = e.target.dataset.action;
         if (action === 'increment') updateState(1);
